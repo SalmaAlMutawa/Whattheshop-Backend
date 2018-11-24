5c136284590488b9a7148d1214f2ade287d8ba44
SalmaAlMutawa <s.almutawa96@gmail.com>
2018-11-24 20:57:54 +0300

    added user fields and added login serializer & view to allow user to login using his email

--- api/serializers.py
+++ api/serializers.py
@@ -15,11 +15,40 @@ class UserCreateSerializer(serializers.ModelSerializer):
         first_name = validated_data['first_name']
         last_name = validated_data['last_name']
         email = validated_data['email']
-        new_user = User(username=username)
+        new_user = User(first_name=first_name, last_name=last_name, email=email, username=username)
         new_user.set_password(password)
         new_user.save()
         return validated_data
-        
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        my_email = data.get('email')
+        my_username = data.get('username')
+        my_password = data.get('password')
+
+        try:
+            user_obj = User.objects.get(email=my_email)
+        except:
+            raise serializers.ValidationError("This email does not exist")
+
+        if not user_obj.check_password(my_password):
+            raise serializers.ValidationError("Incorrect username/password combination!")
+
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(user_obj)
+        token = jwt_encode_handler(payload)
+
+        data["token"] = token
+        return data
+
+
 
 class ItemListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -43,4 +72,3 @@ class ItemDetailSerializer(serializers.ModelSerializer):
             'price',
             'image',
             ]
-
--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from itemsApp.models import Item
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.generics import (
     ListAPIView,
@@ -11,10 +14,23 @@ from .serializers import (
 	UserCreateSerializer,
 	ItemListSerializer,
 	ItemDetailSerializer,
-)
+    UserLoginSerializer,
+ )
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
+
+class UserLoginAPIView(APIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        my_data = request.data
+        serializer = UserLoginSerializer(data=my_data)
+        if serializer.is_valid(raise_exception=True):
+            valid_data = serializer.data
+            return Response(valid_data, status=HTTP_200_OK)
+        return Response(serializer.errors, HTTP_400_BAD_REQUEST)
+
 
 
 
